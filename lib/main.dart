e1992db18f54827530a139d460391e973116714c
Peto <peto@khu.ac.kr>
2024-11-03 09:04:25 +0900

    Add Permission Notification

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,24 @@
 import 'package:flutter/material.dart';
-import 'package:frontend/ui/nav_bar.dart';
 import 'package:provider/provider.dart';
 import 'package:frontend/provider/user_provider.dart';
 import 'package:frontend/ui/register/splash_screen.dart';
+import 'package:flutter_local_notifications/flutter_local_notifications.dart';
+import 'package:firebase_messaging/firebase_messaging.dart';
+import 'package:firebase_core/firebase_core.dart';
 
-void main() {
+// flutter_local_notifications 플러그인 초기화
+final FlutterLocalNotificationsPlugin _local = FlutterLocalNotificationsPlugin();
+
+// 백그라운드 메시지 처리 핸들러
+Future<void> _firebaseMessagingBackgroundHandler(RemoteMessage message) async {
+  await Firebase.initializeApp();
+  _showNotification(message);
+}
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+  FirebaseMessaging.onBackgroundMessage(_firebaseMessagingBackgroundHandler);
   runApp(
     MultiProvider(
       providers: [
@@ -31,3 +45,26 @@ class MyApp extends StatelessWidget {
   }
 }
 
+// 알림 표시 함수
+Future<void> _showNotification(RemoteMessage message) async {
+  const AndroidNotificationDetails androidPlatformChannelSpecifics =
+  AndroidNotificationDetails(
+    'high_importance_channel', // AndroidManifest.xml에 지정한 채널 ID와 동일하게 설정
+    'High Importance Notifications', // 채널 이름
+    importance: Importance.high,
+    priority: Priority.high,
+    showWhen: false,
+  );
+  const NotificationDetails platformChannelSpecifics = NotificationDetails(
+    android: androidPlatformChannelSpecifics,
+  );
+  await _local.show(
+    0, // 알림 ID
+    message.notification?.title,
+    message.notification?.body,
+    platformChannelSpecifics,
+    payload: '알림을 클릭했을 때의 데이터',
+  );
+}
+
+FlutterLocalNotificationsPlugin get localNotificationsPlugin => _local;
--- lib/ui/register/splash_screen.dart
+++ lib/ui/register/splash_screen.dart
@@ -4,6 +4,9 @@ import 'package:flutter_svg/svg.dart';
 import 'package:frontend/const/text_styles.dart';
 import 'dart:async';
 import 'package:frontend/ui/register/carousel_screen.dart';
+import 'package:permission_handler/permission_handler.dart';
+import 'package:frontend/main.dart';
+import 'package:flutter_local_notifications/flutter_local_notifications.dart';
 
 class SplashScreen extends StatefulWidget {
   const SplashScreen({super.key});
@@ -18,6 +21,8 @@ class _SplashScreenState extends State<SplashScreen> {
    @override
   void initState() {
     super.initState();
+    _permissionWithNotification();
+    _initialization();
 
     Timer(const Duration(milliseconds: 1500), () {
       Navigator.of(context).push(
@@ -27,6 +32,28 @@ class _SplashScreenState extends State<SplashScreen> {
       );
     });
   }
+
+   // 로컬 알림 초기화
+   void _initialization() async {
+     const AndroidInitializationSettings android =
+     AndroidInitializationSettings("@mipmap/ic_launcher");
+     const DarwinInitializationSettings ios = DarwinInitializationSettings(
+       requestSoundPermission: false,
+       requestBadgePermission: false,
+       requestAlertPermission: false,
+     );
+     const InitializationSettings settings =
+     InitializationSettings(android: android, iOS: ios);
+     await localNotificationsPlugin.initialize(settings);
+   }
+
+   // 알림 권한 요청
+   void _permissionWithNotification() async {
+     var status = await Permission.notification.status;
+     if (!status.isGranted) {
+       await Permission.notification.request();
+     }
+   }
 
   @override
   Widget build(BuildContext context) {
